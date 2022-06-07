45388180b2af2bfcb01aa4ceba67ee70b7d568aa
nericblue <nericblue@gmail.com>
2022-06-07 16:25:16 +0200

    My last commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result.
+ * @a: An integer a
+ * @b: An integer b
+ * Description: Function that adds two integers and returns the result
+ * Return: Result to add a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
