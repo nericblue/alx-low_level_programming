c46b53656e3659101f148be4cab098e14477374d
nericblue <nericblue@gmail.com>
2022-06-14 02:05:42 +0200

    Corrections

--- 0x05-pointers_arrays_strings/9-main.c
+++ 0x05-pointers_arrays_strings/9-main.c
@@ -12,7 +12,7 @@ int main(void)
 	char s1[98];
 	char *p;
 
-	p = _strcpy(s1, "First things first, and then solve the code.\n");
+	p = _strcpy(s1, "First, solve the problem. Then, write the code\n");
 	printf("%s", s1);
 	printf("%s", p);
 	return (0);
